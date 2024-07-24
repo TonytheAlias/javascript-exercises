6da0b0f012ac2f6aeee39d23f96125e6f325c44a
TonytheAlias <tyreekbrasfield@gmail.com>
2024-07-24 13:59:28 -0500

    Completed helloWorld and repeatString exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+ if(num < 0 ){return "ERROR"}
+ 
+    let multiString = ""
 
+    for(let i = 1; i <= num ; i++){ 
+        multiString += string;
+    }
+ return multiString
 };
 
 // Do not edit below this line
