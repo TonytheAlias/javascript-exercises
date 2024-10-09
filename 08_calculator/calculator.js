ac8b1266aadf11bc7aa95222a7a394fe5565f84e
TonytheAlias <tyreekbrasfield@gmail.com>
2024-10-09 15:58:52 -0500

    Completed calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
+const add = function(num1, num2) {
+	let sum;
+  sum = num1 + num2; 
+  return sum; 
+};
+
+const subtract = function(num1, num2) {
+  let diff;
+  diff = num1 - num2;
+  return diff;
 	
 };
 
-const subtract = function() {
-	
+const sum = function(num) {
+  
+  let sum = num.reduce((total, currentItem) => {
+   return total + currentItem;
+  },0);
+  return sum;
 };
 
-const sum = function() {
-	
+const multiply = function(num) {
+  let multi = num.reduce((total, currentItem) => {
+    return total * currentItem;
+   },1);
+   return multi;
 };
 
-const multiply = function() {
-
+const power = function(num1, num2) {
+	let pow = num1 ** num2;
+  return pow;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  
+  let ans = 1; 
+  for (let i = 2; i <= num; i++) {
+    ans *= i; 
+  }
+  
+  return ans;
 };
 
 // Do not edit below this line
