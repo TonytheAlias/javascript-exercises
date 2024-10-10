dc4ae80bde68e756d9679587d0b9b288528270b7
TonytheAlias <tyreekbrasfield@gmail.com>
2024-10-10 13:05:17 -0500

    Complete palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+   const cleanedStr = str
+   .replace(/[^a-zA-Z0-9]/g, '')
+   .toLowerCase(); 
+ 
+let  reverseStr = cleanedStr.split("").reverse().join("");
 
+ if (cleanedStr === reverseStr){
+    return true;
+ }
+ else{
+    return false;
+ }
 };
 
 // Do not edit below this line
