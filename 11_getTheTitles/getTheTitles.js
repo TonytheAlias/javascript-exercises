8485233b7194590a95247cf3154b63007586607f
TonytheAlias <tyreekbrasfield@gmail.com>
2024-10-11 15:20:44 -0500

    Completed titles and oldest exercises

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+let title = books.map(book => `${book.title}`);
 
+return title;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        const currentYear = new Date().getFullYear();
+        const deathYear = person.yearOfDeath || currentYear; // Use current year if alive
+        const age = deathYear - person.yearOfBirth;
 
+        // Compare ages to find the oldest
+        return age > (oldest.age || 0) ? { ...person, age } : oldest;
+    },0);
 };
 
 // Do not edit below this line
