b4a26790c59cf431b08eff044f65524433ffde3b
TonytheAlias <tyreekbrasfield@gmail.com>
2024-10-10 19:46:56 -0500

    Completed fibonacci sequence excercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+let intNum = parseInt(num);
+const sequence = [0,1];
 
+if(intNum < 0){
+    return 'OOPS';
+}
+else if (intNum === 0){
+    return 0;
+}
+else if (intNum === 1){
+    return 1;
+}
+else {
+    for(let i = 2; i <= intNum; i++){
+        
+        sequence[i] = sequence[i - 1] + sequence[i - 2];
+    }
+    return sequence[intNum];
+}
 };
 
 // Do not edit below this line
